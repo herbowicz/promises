dcc0d0a7f179a59e303f9b14c30fa47d3c927a1d
herbowicz-coderslab <grzegorz.herbowicz@coderslab.pl>
2018-01-18 18:59:50 +0100

    test1

--- tests/problem1.test.js
+++ tests/problem1.test.js
@@ -2,20 +2,31 @@
  * Napisz funkcję 'negation', która przyjmuje Promise'a jako argument i zwraca Promise'a, który
  * jest jego negacją, tzn. rozwiązuje się poprawnie, gdy Promise wejściowy kończy się
  * niepowodzeniem a kończy się niepowodzeniem, gdy Promise wejściowy zwraca poprawną wartość.
- * 
+ *
  * Przykładowo Promise zwracany przez
- * 
+ *
  * negation(Promise.resolveWith(5).after(50));
- * 
+ *
  * powinien rzucać liczbę 5 jako obiekt błędu, zaś
- * 
+ *
  * negation(Promise.rejectWith(5).after(50));
- * 
+ *
  * powinien poprawnie zwracać liczbę 5 jako wartość.
- * 
+ *
  */
 
 const p = require('../utils.js');
+
+function negation(myPromise){
+  return new Promise(function(resolve, reject) {
+      myPromise.then(function(result) {
+        reject(result);
+      });
+      myPromise.catch(function(result) {
+        resolve(result);
+      });
+  });
+}
 
 describe('problem1', () => {
     it('negates resolved promises', (done) => {
